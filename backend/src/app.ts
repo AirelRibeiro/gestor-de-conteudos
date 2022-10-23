29dee85e46c335422ccd5f7fd8fc1004fc1430a9
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-23 20:15:05 -0300

    feat: inclui middleware de erros e importa express-async-errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import contentRoute from './routes/Content.route';
+import errorMiddleware from './middlewares/errorMiddleware';
+require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
 
 app.use(contentRoute);
+
+app.use(errorMiddleware);
 
 export default app;
