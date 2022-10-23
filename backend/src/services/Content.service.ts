92132e68cd40b6134d6479eca6e281e80e6f75fa
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-23 15:52:39 -0300

    feat: creates Content service

---
+++ backend/src/services/Content.service.ts
@@ -0,0 +1,50 @@
+import ContentModel from '../database/models/Content';
+import HistoryModel from '../database/models/History';
+
+class ContentService {
+  constructor(
+    private contentModel: typeof ContentModel,
+    private historyModel: typeof HistoryModel
+  ) {}
+
+  async create(content: ContentModel): Promise<ContentModel> {
+    const createdContent = await this.contentModel.create(content as any);
+    return this.historyModel.create({
+      content: createdContent.id,
+      titulo: createdContent.titulo,
+      corpo: !content.corpo ? 'Campo deixado vazio pelo autor vazio.' : content.corpo
+    }).then((result) => result );
+  }
+
+  async findAll(): Promise<ContentModel[]> {
+    const contents = await this.contentModel.findAll();
+    return contents;
+  }
+
+  async findByPk(id: number): Promise<ContentModel> {
+    const content = await this.contentModel.findByPk(id);
+
+    if (!content) throw new Error('contentNotFound');
+
+    return content
+  }
+
+  async update(id: number, content: ContentModel): Promise<object> {
+    return this.contentModel.update(content, { where: { id } }).then(async () => {
+      this.historyModel.create({
+        content: id,
+        titulo: content.titulo,
+        corpo: !content.corpo ? 'Campo deixado vazio pelo autor vazio.' : content.corpo
+      });
+      return { message: 'Conteúdo atualizado com sucesso!' }
+    }).catch(() => {throw new Error('contentNotFound')});
+  }
+
+  async delete(id: number): Promise<object> {
+    return this.contentModel.destroy({ where: { id } }).then(() => {
+      return { message: 'Conteúdo excluído com sucesso!' }
+    }).catch(() => {throw new Error('contentNotFound')});
+  }
+}
+
+export default ContentService;
