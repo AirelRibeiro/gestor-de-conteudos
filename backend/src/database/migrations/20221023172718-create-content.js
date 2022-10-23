4788b9f2aea9ef7827488e02addc6ad6171f53ea
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-23 14:32:11 -0300

    feat: gera migration content com sequelize-cli

---
+++ backend/src/database/migrations/20221023172718-create-content.js
@@ -0,0 +1,32 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Contents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      titulo: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      corpo: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Contents');
+  }
+};
