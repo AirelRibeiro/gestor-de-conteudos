c50ecf48008ad6d5456f3a306f4feb4058b7cd5b
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-23 14:41:55 -0300

    feat: inclui referência a tabela content

---
+++ backend/src/database/migrations/20221023173707-create-history.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Histories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      titulo: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      corpo: {
+        type: Sequelize.STRING
+      },
+      contentId: {
+        allowNull: false,
+        type: Sequelize.NUMBER,
+        references: {
+          model: 'Contents',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at',
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Histories');
+  }
+};
