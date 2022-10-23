e7943cfc06dedf8482667058b5c03fc25c4e7b29
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-23 15:53:36 -0300

    feat: inclui config.ts exportando configurações do Sequelize

---
+++ backend/src/database/config/config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { Options } from 'sequelize';
+
+const config: Options = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '123456',
+  database: process.env.DB_NAME || 'Motrix',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT || 3002),
+  dialect: 'mysql',
+}
+
+module.exports = config;
