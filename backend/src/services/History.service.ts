c02c0b5b0228c0787be8d812bb3e9d294d94cc96
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-23 15:52:56 -0300

    feat: creates History service

---
+++ backend/src/services/History.service.ts
@@ -0,0 +1,16 @@
+import HistoryModel from '../database/models/History';
+
+class HistoryService {
+  constructor(
+    private historyModel: typeof HistoryModel
+  ) {}
+  
+  async findHistoryById(id: number): Promise<HistoryModel[]> {
+    const history = await this.historyModel.findAll({ where: { content: id } });
+    return history;
+  }
+
+  
+}
+
+export default HistoryService;
