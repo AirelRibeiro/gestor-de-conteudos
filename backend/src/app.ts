8863ac97490cecbe7a94d6e7ff46b5e2707ab9fc
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-24 20:52:07 -0300

    feat: inclui cors package

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import contentRoute from './routes/Content.route';
 import errorMiddleware from './middlewares/errorMiddleware';
 require('express-async-errors');
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(contentRoute);
 
