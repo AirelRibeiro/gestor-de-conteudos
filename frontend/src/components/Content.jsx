3c712c27f4fafae4d08df056cceb17c9b245f103
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-25 11:33:08 -0300

    fix: corrigee nome do botão Excluir

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -25,7 +25,7 @@ export default function Content(props) {
         />
       <input
         type="button"
-        value="Excluirr"
+        value="Excluir"
         className="button"
         onClick={deleteFunction}
         />
