386d4bfac49830f7a289d8290603e1d34cd78e9f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-25 20:09:27 -0300

    refctor: troca rota delete por rota search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Create from './pages/Create';
-import Delete from './pages/Delete';
+import Search from './pages/Search';
 import History from './pages/History';
 import Home from './pages/Home';
 import Update from './pages/Update';
+import './style/App.css';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <div>
       <Routes>
         <Route path="/create" element={<Create />} />
-        <Route path="/delete" element={<Delete />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/update/:id" element={<Update />} />
         <Route path="/:id" element={<History />} />
         <Route path="/" element={<Home />} />
