27b0a491987c5d4b436a92001277b7139a7d2309
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-24 22:20:13 -0300

    feat: cria página de histórico

---
+++ frontend/src/pages/History.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Content from '../components/Content';
+import Updates from '../components/Updates';
+import { requestDelete, requestHistory } from '../helpers/apiHelpers';
+
+function History() {
+  const [information, setInformation] = useState([]);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData(id) {
+      const data = await requestHistory(id);
+      setInformation([data.content, data.history]);
+    }
+    fetchData(location.pathname);
+  }, [location]);
+
+  async function deleteFunction() {
+    const id = location.pathname.split('/')[1];
+    const result = await requestDelete(id);
+    alert(result.message);
+    navigate('../')
+  }
+
+  return (
+    <div className="history">
+      { information.length && <Content content={information[0]} update={navigate} deleteFunction={deleteFunction} /> }
+      { information.length && <Updates updates={information[1]} /> }
+    </div>
+  );
+}
+
+export default History;
