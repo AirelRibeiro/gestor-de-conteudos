526f696eddbc669c2251bf91253b3b90891438c4
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-25 20:11:35 -0300

    feat: inclui opção para exclusão "em massa" na home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { requestDelete, requestGetAll } from "../helpers/apiHelpers";
+import '../style/Home.css';
 
 function Home() {
   const [information, setInformation] = useState([]);
+  const [checked, setChecked] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,35 +17,66 @@ function Home() {
     fetchData();
   }, []);
 
-  async function deleteFunction(id) {
+  async function deleteOne(id) {
     const result = await requestDelete(id);
     alert(result.message);
     window.location.reload()
   }
 
+  async function deleteMany() {
+    const result = await Promise.all(checked.map((id) => requestDelete(id)));
+    alert(`${result.length} conteúdos foram excluídos com sucesso!`);
+    window.location.reload()
+  }
+
+  async function checkFunction(id) {
+    if(checked.includes(id)) {
+      const newChekedList = checked.filter((check) => check !== id);
+      return setChecked(newChekedList);
+    }
+    return setChecked([...checked, id]);
+  }
+
   return (
-    <div className="home">
-      <h1>Home</h1>
+    <div id="home">
+      <h1>Conteúdos</h1>
+      <input
+            type="button"
+            value="Excluir conteúdos selecionados"
+            id="delete-button"
+            onClick={deleteMany}
+          />
       {information.length && (
-        <div>
+        <div id="home-contents">
           {
             information.map((content) => (
-              <div>
+              <div className="home-single-content">
                 <h2>{ content.titulo }</h2>
-                <p>{content.id}</p>
                 <p>{content.corpo}</p>
-                <input
-                  type="button"
-                  value="Mais informações"
-                  className="button"
-                  onClick={() => navigate(`/${content.id}`)}
-                />
-                <input
-                  type="button"
-                  value="Excluir"
-                  className="button"
-                  onClick={() => deleteFunction(content.id)}
-                />
+                <div>
+                  <input
+                    type="button"
+                    value="Mais informações"
+                    className="home-button"
+                    onClick={() => navigate(`/${content.id}`)}
+                  />
+                  <input
+                    type="button"
+                    value="Excluir"
+                    className="home-button"
+                    onClick={() => deleteOne(content.id)}
+                  />
+                </div>
+                <label for="check-button">
+                  Marque para exclusão
+                  <input
+                    type="checkbox"
+                    id="check-button"
+                    value={content.id}
+                    className="checkebox"
+                    onClick={() => checkFunction(content.id)}
+                  />
+                </ label>
               </div>
             ))
           }
