1c4359c191d6f0d34dad1cb6c2da682d8a1fc2d3
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-24 22:20:02 -0300

    feat: cria página de criação de conteúdo

---
+++ frontend/src/pages/Create.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ContentForm from '../components/ContentForm';
+import { requestcreate } from '../helpers/apiHelpers';
+
+function Create() {
+  const navigate = useNavigate();
+  const [content, setContent] = useState({ title: '', body: '' });
+
+  async function createContent() {
+    const { title, body } = content;
+    let insertedContent;
+    if(!body) {
+      insertedContent = await requestcreate({ titulo: title });
+    } else {
+      insertedContent = await requestcreate({ titulo: title, corpo: body });
+    }
+    setContent({ title: '', body: '' });
+    navigate(`/${insertedContent.content_id}`);
+  }
+
+  function change({target}) {
+    const { name, value } = target;
+    setContent({ ...content, [name]: value })
+  }
+
+  return (
+    <div className="create">
+      <h1>Create</h1>
+      <ContentForm
+        onChange={change}
+        onClick={createContent}
+        plchT="Título"
+        plchB="Conteúdo"
+        title={content.title}
+        body={content.body}
+      />
+    </div>
+  );
+}
+
+export default Create;
