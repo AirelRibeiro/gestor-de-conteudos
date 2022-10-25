c8ed7fa33dd2d7bb342f3762effd38cb4824cc66
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-24 22:14:30 -0300

    feat: configura App com rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Create from './pages/Create';
+import History from './pages/History';
+import Home from './pages/Home';
+import Update from './pages/Update';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">  
+      <Header />
+      <div>
+      <Routes>
+        <Route path="/create" element={<Create />} />
+        <Route path="/update/:id" element={<Update />} />
+        <Route path="/:id" element={<History />} />
+        <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 }
