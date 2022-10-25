1552741e4c6c0db325d1612436154fcae1b6a817
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-24 22:17:49 -0300

    feat: cria componente Content para renderizar conteúdos individuais

---
+++ frontend/src/components/Content.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function Content(props) {
+  const { content, update, deleteFunction } = props;
+  return (
+    <div>
+      <h1>{content.titulo}</h1>
+      <p>
+        <strong>Conteúdo: </strong>
+        {` ${content.corpo ? content.corpo : 'Sem conteúdo'}`}
+      </p>
+      <p>
+        <strong>Conteúdo criado em: </strong>
+        {` ${new Date(content.created_at)}`}
+      </p>
+      <p>
+          <strong>Última atualização: </strong>
+          {` ${new Date(content.updated_at)}`}
+      </p>
+      <input
+        type="button"
+        value="Atualizar"
+        className="button"
+        onClick={() => update(`/update/${content.id}`)}
+        />
+      <input
+        type="button"
+        value="Excluirr"
+        className="button"
+        onClick={deleteFunction}
+        />
+    </div>
+  );
+}
