7ef170300a98c25737c2ba181051f2436a3ff6d1
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-24 22:16:49 -0300

    feat: cria funções de chamar API:
    requestGetAll: para buscar todos os conteúdos
    requestDelete: para excluir o conteúdo
    requestUpdate: para atualizar o conteúdo
    requestHistory: para pegar histórico de atualização do conteúdo
    requestcreate: para criar um conteúdo no banco de dados

---
+++ frontend/src/helpers/apiHelpers.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: `http://localhost:3001`,
+});
+
+export const requestGetAll = async () => {
+  const { data } = await API.get('/');
+  return data;
+};
+
+export const requestDelete = async (id) => {
+  const { data } = await API.delete(`/${id}`);
+  return data;
+}
+
+export const requestUpdate = async (id, body) => {
+  const { data } = await API.put(`/${id}`, body);
+  return data;
+};
+
+export const requestHistory = async (id) => {
+  const { data } = await API.get(id);
+  return data;
+};
+
+export const requestcreate = async (body) => {
+  const { data } = await API.post('/', body);
+  return data;
+};
+
+export default API;
