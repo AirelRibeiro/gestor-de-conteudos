8d5b8098542feedeaa01740fbc1fe3804caebb9b
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-26 15:51:07 -0300

    test: cria mocks para testes de integração

---
+++ backend/src/tests/mocks/History.mock.ts
@@ -0,0 +1,29 @@
+export const historyOfIntert = {
+  id: 4,
+  content_id: 4,
+  titulo: 'É nosso dever conscientizar alunos contra LGBTfobia',
+  corpo: `Desde 2013, está em andamento um projeto de conscientização contra a LGBTfobia na escola estadual Professor Joaquim Luiz de Brito, na zona oeste de São Paulo.
+  Idealizado pelo professor Fábio de Lima, a iniciativa tem a intenção de informar os alunos, e a comunidade como um todo, sobre a importância do debate sobre diversidade sexual e de gênero.
+  “É nosso dever como ser humano fazer algo em prol da sociedade, nós, como educadores, temos o campo fértil para conscientizar os alunos a importância dos valores humanos”, afirmou Fábio`,
+  created_at: '2022-10-26T08:30:57.000Z',
+  updated_at: '2022-10-26T08:30:57.000Z'
+}
+
+export const historyById = [
+  {
+    id: 1,
+    content_id: 1,
+    titulo: 'Jovens de Paraisópolis, em São Paulo, recebem bolsa de iniciação científica',
+    corpo: 'Medicina, biologia, engenharia, economia, política. Estas são apenas algumas das áreas que podem abrigar o trabalho de um cientista. Embora seja muito comum relacionar a profissão ao ambiente típico de laboratório, cientistas estão espalhados nos mais diversos campos do conhecimento.',
+    created_at: '2022-10-24T10:25:27.000Z',
+    updated_at: '2022-10-24T10:25:27.000Z'
+  },
+  {
+    id: 8,
+    content_id: 19,
+    titulo: 'Jovens de Paraisópolis recebem bolsa de iniciação científica',
+    corpo: 'Com o objetivo de tornar o conhecimento científico acessível a estudantes da educação básica da rede pública e de sensibilizar novas gerações para a carreira acadêmico-científica, o Hospital Albert Einstein disponibiliza 15 bolsas de Iniciação Científica júnior para jovens de Paraisópolis, em São Paulo.',
+    created_at: '2022-10-26T11:21:12.000Z',
+    updated_at: '2022-10-26T11:21:12.000Z'
+  }
+]
