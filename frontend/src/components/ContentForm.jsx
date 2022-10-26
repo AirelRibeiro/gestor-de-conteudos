ae93472a36d521a232955e342e3c59d120c129dd
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-26 16:21:23 -0300

    feat: inclui demosntração prévia de conteúdo para págias de criação e atualização

--- frontend/src/components/ContentForm.jsx
+++ frontend/src/components/ContentForm.jsx
@@ -11,7 +11,8 @@ function ContentForm(props) {
   } = props
 
   return (
-      <form className="form">
+      <div id="form-page">
+        <form className="form">
           <input
             type="text"
             name="title"
@@ -35,9 +36,37 @@ function ContentForm(props) {
           value="Salvar"
           className="form-button"
           onClick={onClick}
-          disabled={title.length < 5}
+          disabled={title.length < 5 || title.length > 100}
         />
       </form>
+      <div id="content-demostration">
+      <h2>{ title }</h2>
+      <p>{ body }</p>
+      <div>
+        <input
+          type="button"
+          value="Mais informações"
+          className="form-button"
+          onClick={() => alert('Recurso meramente ilustrativo!')}
+        />
+        <input
+          type="button"
+          value="Excluir"
+          className="form-button"
+          onClick={() => alert('Recurso meramente ilustrativo!')}
+        />
+      </div>
+      <label for="check-button">
+        Marque para exclusão
+        <input
+          type="checkbox"
+          id="check-button"
+          className="checkebox"
+          onClick={() => alert('Recurso meramente ilustrativo!')}
+        />
+      </ label>
+    </div>
+      </div>
   );
 }
 
