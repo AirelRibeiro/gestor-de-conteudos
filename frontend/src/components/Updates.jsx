b99538577613ff90b850625a470a66d84d77fae9
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-27 13:20:38 -0300

    feat: inclui possibilidade para retornar a versão anterior do conteúdo

--- frontend/src/components/Updates.jsx
+++ frontend/src/components/Updates.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { requestUpdate } from '../helpers/apiHelpers';
 
 export default function Updates(props) {
   const { updates } = props;
+
+  async function updateContent(title, body, id) {
+    if(!body) {
+      await requestUpdate(id, { titulo: title });
+    } else {
+      await requestUpdate(id, { titulo: title, corpo: body });
+    }
+    alert('Versão de conteúdo recuperada com sucesso!');
+    window.window.location.reload()
+  }
+
   return (
     <><h1 id="history-title">Histórico de atualizações</h1><div id="updates">
       {updates.map((content, i) => (
@@ -16,6 +28,12 @@ export default function Updates(props) {
             <strong>Atualização feita em: </strong>
             {` ${new Date(content.created_at)}`}
           </p>
+          <input
+          type="button"
+          value="Voltar para esta versão"
+          className="back-button"
+          onClick={() => updateContent(content.titulo, content.corpo, content.content_id)}  
+          />
         </div>
       ))}
     </div></>
