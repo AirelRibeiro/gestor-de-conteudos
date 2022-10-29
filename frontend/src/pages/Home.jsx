708edb072eab9504d038c01f409d3dea154a5c63
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-10-29 18:28:51 -0300

    refactor: inclui componente ContentCards

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,11 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { requestDelete, requestGetAll } from "../helpers/apiHelpers";
+import { useEffect, useState } from 'react';
+import ContentCard from '../components/ContentCards';
+import { requestDelete, requestGetAll } from '../helpers/apiHelpers';
 import '../style/Home.css';
 
 function Home() {
   const [information, setInformation] = useState([]);
   const [checked, setChecked] = useState([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchData() {
@@ -30,7 +29,7 @@ function Home() {
   }
 
   async function checkFunction(id) {
-    if(checked.includes(id)) {
+    if (checked.includes(id)) {
       const newChekedList = checked.filter((check) => check !== id);
       return setChecked(newChekedList);
     }
@@ -41,46 +40,17 @@ function Home() {
     <div id="home">
       <h1>Conteúdos Motrix</h1>
       <input
-            type="button"
-            value="Excluir conteúdos selecionados"
-            id="delete-button"
-            onClick={deleteMany}
-          />
+        type="button"
+        value="Excluir conteúdos selecionados"
+        id="delete-button"
+        onClick={deleteMany}
+      />
       {information.length && (
-        <div id="home-contents">
-          {
-            information.map((content) => (
-              <div className="home-single-content">
-                <h2>{ content.titulo }</h2>
-                <p>{content.corpo}</p>
-                <div>
-                  <input
-                    type="button"
-                    value="Mais informações"
-                    className="home-button"
-                    onClick={() => navigate(`/${content.id}`)}
-                  />
-                  <input
-                    type="button"
-                    value="Excluir"
-                    className="home-button"
-                    onClick={() => deleteOne(content.id)}
-                  />
-                </div>
-                <label for="check-button">
-                  Marque para exclusão
-                  <input
-                    type="checkbox"
-                    id="check-button"
-                    value={content.id}
-                    className="checkebox"
-                    onClick={() => checkFunction(content.id)}
-                  />
-                </ label>
-              </div>
-            ))
-          }
-        </div>
+        <ContentCard
+          information={information}
+          deleteOne={deleteOne}
+          checkFunction={checkFunction}
+        />
       )}
     </div>
   );
